d6a01b0799b2d52f586548c35a2c9a0ce2cfed4a
Jakub Baranowski <jbaranowski@gog.com>
2021-06-28 20:44:19 +0200

    Removed remnants of mission command

--- fileops/files.go
+++ fileops/files.go
@@ -7,13 +7,13 @@ import (
 	"github.com/JakBaranowski/gb-tools/common"
 )
 
-// Copies file and it's content from a file at srcPath to a file at dstPath.
-func Copy(srcPath string, dstPath string) {
-	src, err := os.Open(srcPath)
+// Copies file and it's content from a file at sourcePath to a file at dstPath.
+func Copy(pathToSource string, pathToDestination string) {
+	src, err := os.Open(pathToSource)
 	common.Must(err)
 	defer src.Close()
 
-	dst, err := os.Create(dstPath)
+	dst, err := os.Create(pathToDestination)
 	common.Must(err)
 	defer dst.Close()
 
@@ -21,9 +21,9 @@ func Copy(srcPath string, dstPath string) {
 	common.Must(err)
 }
 
-// Touches, i.e. creates an empty file, at dstPath.
-func Touch(dstPath string) {
-	dst, err := os.Create(dstPath)
+// Touches, i.e. creates an empty file, at destinationPath.
+func Touch(pathToDestination string) {
+	dst, err := os.Create(pathToDestination)
 	common.Must(err)
 	defer dst.Close()
 }
@@ -37,82 +37,36 @@ func OpenAndReadFile(pathToFile string) []byte {
 }
 
 // Reads a provided file. Returns read byte array.
-func ReadFile(file *os.File) []byte {
+func ReadFile(file *os.File) (fileByte []byte) {
 	fileStat, err := file.Stat()
 	common.Must(err)
-
-	fileByte := make([]byte, fileStat.Size())
+	fileByte = make([]byte, fileStat.Size())
 	_, err = file.Read(fileByte)
 	common.Must(err)
-
 	return fileByte
 }
 
-// Checks if directory under path exist, and creates it if it does not.
-func CreateDirIfDoesntExist(path string, perm os.FileMode) {
-	if !DoesExist(path) {
-		CreateDir(path, perm)
+// Checks if directory under pathToDir exist, and creates it if it does not.
+func CreateDirIfDoesntExist(pathToDir string, perm os.FileMode) {
+	if !DoesExist(pathToDir) {
+		CreateDir(pathToDir, perm)
 	}
 }
 
-// Checks if a directory or file exists under specified path
-func DoesExist(path string) bool {
-	_, err := os.Stat(path)
+// Checks if a directory or file exists under specified pathToCheck.
+func DoesExist(pathToCheck string) bool {
+	_, err := os.Stat(pathToCheck)
 	return !os.IsNotExist(err)
 }
 
-// Creates directory at path with the specified permission.
-func CreateDir(path string, perm os.FileMode) {
-	err := os.Mkdir(path, perm)
+// Creates directory at pathToDir with the specified permission.
+func CreateDir(pathToDir string, perm os.FileMode) {
+	err := os.Mkdir(pathToDir, perm)
 	common.Must(err)
 }
 
-// Removes the file at path.
-func RemoveFile(path string) {
-	err := os.Remove(path)
-	common.Must(err)
-}
-
-// Creates a copy of the srcFile in dstFile, and replaces all occurences of
-// Will keep the file size unchanged if keepSize is True.
-func ReplaceBytesNewFile(
-	srcFile string,
-	dstFile string,
-	find string,
-	replace string,
-	keepSize bool,
-) {
-	byteFind := []byte(find)
-	byteReplace := []byte(replace)
-
-	byteSrc := OpenAndReadFile(srcFile)
-	byteDst := ReplaceBytes(byteSrc, byteFind, byteReplace, keepSize)
-
-	dst, err := os.Create(dstFile)
-	common.Must(err)
-
-	_, err = dst.Write(byteDst)
-	common.Must(err)
-}
-
-// Replaces all occurences of stringFind in the specified file with stringReplace.
-// Will keep the file size unchanged if keepSize is True.
-func ReplaceBytesInFile(
-	file string,
-	find string,
-	replace string,
-	keepSize bool,
-) {
-	byteFind := []byte(find)
-	byteReplace := []byte(replace)
-
-	src, err := os.OpenFile(file, os.O_RDWR, 0644)
-	common.Must(err)
-	defer src.Close()
-
-	byteSrc := ReadFile(src)
-	byteDst := ReplaceBytes(byteSrc, byteFind, byteReplace, keepSize)
-
-	_, err = src.WriteAt(byteDst, 0)
+// Removes the file at pathToFile.
+func RemoveFile(pathToFile string) {
+	err := os.Remove(pathToFile)
 	common.Must(err)
 }
